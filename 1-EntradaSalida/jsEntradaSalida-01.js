6d79fac8bc0ba478c0508ed52822b24df466e8ea
Your Name <you@example.com>
2020-07-14 12:20:21 -0300

    1 EyS ejers 1 al 5

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,10 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Delgadop");
+
+	// alert("Delgadop");
+    // confirm("desea continuar?");
+	prompt("ingrese su nombre");
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,22 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato;
+
+	dato = prompt ("ingrese un dato");
+	alert(dato);
+
+
+
+
+
+	//var nombre;//underfinded  quiere decir que a esa variable nunca se le asigno un valor
+
+	//nombre = "camila";//string
+	//nombre = null;// cuadno le saco el valor, tuvo un valor pro ahora no 
+	//nombre = 12; // number-- NaN, ifiity
+	//nombre = true; // boolean
+	//alert (nombre);
+
 }
 
